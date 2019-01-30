70866b5ddc0d338faa62b4c2233996855daa5804
thanglongsp <thanglongnguyensp@gmail.com>
2019-01-30 16:31:01 +0700

    Fix ui home and profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,57 @@
 @extends('layouts.master')
+@section('content')
+@include('layouts.slide')
+@include('layouts.header')
+<div class="col-sm-12">
+        <div class="col-sm-4">
+            <div class="myDiv">
+                <div class="myDivChild">
+                    <p>Chọn bệnh viện</p>
+                </div>
+                <select>
+                    <option value="#">Tất cả</option>
+                    <option value="#">Bệnh viện A</option>
+                    <option value="#">Bệnh viện B</option>
+                </select>
+            </div>
+            <br>
+            <div class="myDiv">
+                <div class="myDivChild">
+                    <p>Chọn Khoa</p>
+                </div>
+                <select>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                </select>
+            </div>
+            <br>
+            <div class="myDiv">
+                <div class="myDivChild">
+                    <p>Chọn chế độ khám</p>
+                </div>
+                <select>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                    <option value="#">X</option>
+                </select>
+            </div>
+            <br>
+            <div class="myDiv">
+                <div class="myDivChild">
+                    <p>Chọn ngày giờ</p>
+                </div>
+                <p> Chọn ngày :</p><input class="input" type="date">
+                <p> Chọn giờ : <input type="time">
+            </div>
+        </div>
+        @include('layouts.result')
+    </div>            
--- resources/views/layouts/result.blade.php
+++ resources/views/layouts/result.blade.php
@@ -1,7 +1,25 @@
 <div  class="col-sm-8">
     <div class="myDivResult">
-        <center>
-            <p>Result</p>
-        </center>
+        <br>
+        <h2> Kết quả tìm kiếm :</h2>
+        <br>
+        <div class="myDivResultChild">
+            <ul>
+                <li>Bệnh viện : </li>
+                <li>Khoa : </li>
+                <li>Thời gian :</li>
+                <li>Trạng thái : </li>
+                <li>Chế độ : </li>
+            </ul>
+            <p> <a href="#" style="margin-left:90%;">Chi tiết</a>
+        </div>
+        <div class="myDivResultChild"></div>
+        <ul class="pagination">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+        </ul>
     </div>
 </div>
