d4f1766cd713c064a5ebde1b2240741f879b0776
thanglongsp <thanglongnguyensp@gmail.com>
2019-02-03 01:24:32 +0700

    fix UI forum page

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -2,6 +2,8 @@
 @include('layouts.header')
 @section('content')
 <div class="col-sm-12" style="margin-top: 50px;">
+    <div class="col-sm-1"></div>
+    <!-- Link left -->
     <div class="col-sm-2" style="background-color: red;">
         <div class="divForum">
             <div class="divForumChild"> 
@@ -27,36 +29,89 @@
             <a href="">link ... </a><br>
         </div>
     </div>
-    <div class="col-sm-10">
+    <!-- End link left -->
+    <div class="col-sm-5">
 
         <!-- Đăng bài -->
         <div class="col-sm-12">        
-            <div class="col-sm-1">
+            <div class="col-sm-4">
                 <img type="hidden" class="mr-3 img-responsive" src="{{asset('images/avatars/'.Auth::user()->avatar)}}" alt="Generic placeholder image" style="background: #F0FFFF; border-radius: 50%; border: 2px solid #1E90FF; height: 70px; text-align: center; width: 70px;">
             </div> 
-            <div class="col-sm-10" style="margin-top: 20px;">
-                <a href="#" data-toggle="modal" data-target="#myModalMyPost"><u><i>{{ Auth::user()->full_name }} bạn có muốn đăng bài ?</i></u></a> 
+            <div class="col-sm-8" style="margin-top: 20px;">
+                <a href="#" data-toggle="modal" data-target="#myModalMyPost" style="margin-left:-100px;"><u><i>{{ Auth::user()->full_name }} bạn có muốn đăng bài ?</i></u></a> 
             </div>
         </div>
         <!-- end Đăng bài -->
-        
-        <!-- Bài viết -->
-        <div class="divPostUser">
-            <div class="divPostUserHeader">
-                <p>ccc</p>
+        <div class="content">
+            <!-- Bài viết -->
+            <div class="divPostUser">
+                <div class="divPostUserHeader">
+                    <p>ccc</p>
+                </div>
+                <div class="divPostUserContent">
+                    <p>ccc</p>
+                    <p>ccc</p>
+                    <p>ccc</p>
+                    <p>ccc</p>
+                </div>
+                <div class="divPostUserFooter">
+                    <a href="#"><span class="glyphicon glyphicon-heart" style="font-size:25px; color:red;"></span></a>
+                    <a data-toggle="collapse" data-parent="#accordion" href="#comment"><span class="glyphicon glyphicon-envelope" style="font-size:25px; color:blue;"></span></a>
+                </div>
+
+                <!-- Bình luận -->
+                <div id="comment" class="panel-collapse collapse">
+                    <div class="col-sm-2">
+                        <a class="pr-3">
+                            <img class="mr-3 img-responsive" src="{{asset('images/avatars/'.Auth::user()->avatar)}}" alt="Generic placeholder image" style="background: #F0FFFF; border-radius: 50%; border: 2px solid #1E90FF; height: 50px; text-align: center; width: 50px;">
+                        </a>
+                    </div>
+                    <form action="#" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <div class="col-sm-7">
+                            <input type="text" style="margin-top:10px; margin-left:-45px;" placeholder="Bình luận ... ">
+                        </div>
+                        <div class="col-sm-2" style="margin-left:-50px;">
+                            <a href="#"><span style="margin-top:10px; font-size: 25px;" class="glyphicon glyphicon-picture"></span></a>
+                            <a href="#"><span style="margin-top:10px; font-size: 25px;" class="glyphicon glyphicon-camera"></span></a>
+                        </div>
+                        <div class="col-sm-1">
+                            <button class="btn btn-primary" type="submit" style="margin-top:10px; margin-left:-30px;">Bình luận</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- end bình luận -->
             </div>
-            <div class="divPostUserContent">
-                <p>ccc</p>
-                <p>ccc</p>
-                <p>ccc</p>
-                <p>ccc</p>
-            </div>
-            <div class="divPostUserFooter">
-                <p>sss</p>
-            </div>
+            <!-- end Bài viết -->
         </div>
-        <!-- end Bài viết -->
     </div>
+    <!-- Link right -->
+    <div class="col-sm-2" style="background-color: red;">
+        <div class="divForum">
+            <div class="divForumChild"> 
+                <br>
+                <p>Bài viết được quan tâm</p>
+            </div>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+            <a href="">link ... </a><br>
+        </div>
+    </div>
+    <!-- end link right -->
 </div>
 <!-- Modal -->
 <div class="modal fade" id="myModalMyPost" role="dialog">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script href="{{asset('js/jquery.js')}}"></script> 
